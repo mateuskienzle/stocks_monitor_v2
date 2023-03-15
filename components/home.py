c0325e8db3153391ef580ee64d7e1943ac1bc4a2
Mateus <mateuskienzle@gmail.com>
2023-03-15 19:29:46 -0300

    Alteração nas cores principais do layout, inserção dos graficos nos cards da segunda linha

--- components/home.py
+++ components/home.py
@@ -108,8 +108,12 @@ layout = dbc.Container([
                         dbc.CardBody([
                             dbc.Row([
                                 dbc.Col([
-                                    html.Legend('TOTAL DA CARTEIRA', className='textoSecundario'),
-                                    html.Legend('R$2500.00', className='textoSecundario')
+                                    html.Legend('CARTEIRA: R$2500.00', className='textoSecundario'),
+                                ])
+                            ]),
+                            dbc.Row([
+                                dbc.Col([
+                                    html.Legend('IBOV: R$102.932,38', className='textoQuartenario')
                                 ])
                             ])
                         ])
@@ -122,7 +126,7 @@ layout = dbc.Container([
                     
                 ],md=12, id='cards_ativos'),
             ],  className='g-2 my-auto')
-        ], xs=4, md=9),
+        ], xs=12, md=9),
         dbc.Col([
             dbc.Card([
                 dbc.CardBody([
@@ -154,7 +158,7 @@ layout = dbc.Container([
                         value='1y',
                         id="period_input",
                         inline=True,
-                        className='textoTerciarioOpaco',
+                        className='textoTerciario',
                     ),
                 ], sm=12, md=7),
                 dbc.Col([
@@ -177,7 +181,7 @@ layout = dbc.Container([
             # ]),
             # dbc.Row([
             #     dbc.Col([
-                dcc.Graph(id='line_graph', config={"displayModeBar": False, "showTips": False}, className='graph_line scorecard__graph')    
+                dcc.Graph(id='line_graph', config={"displayModeBar": False, "showTips": False}, className='graph_line')    
             ], xs=12, md=12,)
         #     ])
         # ]),
@@ -224,7 +228,7 @@ def radar_graph(book_data, comparativo):
     else:
         df_total_ibov = df_ibov.groupby('Setor')['Participação'].sum() * 100
         fig = go.Figure()
-        fig.add_trace(go.Scatterpolar(r=df_total_ibov, theta=df_total_ibov.index, name='', fill='toself',
+        fig.add_trace(go.Scatterpolar(r=df_total_ibov, theta=df_total_ibov.index, name='Carteira', fill='toself',
                                     hovertemplate ='<b>IBOV</b>'+'<br><b>Participação</b>: %{r:.2f}%'+ '<br><b>Setor</b>: %{theta}<br>', line=dict(color=LINHAS_PREENCHIMENTO_1)))
 
     fig.update_traces(line={'shape': 'spline'})
@@ -249,6 +253,11 @@ def func_card1(dropdown, period, profit_switch, book_info, historical_info):
     if type(dropdown) != list: dropdown = [dropdown]
     dropdown = ['BVSPX'] + dropdown
     
+    # print('CHEGOU AQUI')
+    # print(dropdown)
+    # import pdb
+    # pdb.set_trace()
+
     df_hist = pd.DataFrame(historical_info)
     df_hist['datetime'] = pd.to_datetime(df_hist['datetime'], format='%Y-%m-%d %H:%M:%S')
     df_hist = slice_df_timedeltas(df_hist, period)
@@ -297,10 +306,11 @@ def atualizar_dropdown(book):
     Output('cards_ativos', 'children'),
     Input('book_data_store', 'data'),
     Input('period_input', 'value'),
+    Input('dropdown_card1', 'value'),
     State('historical_data_store', 'data')
 )
 
-def atualizar_cards_ativos(book_data, period, historical_data):
+def atualizar_cards_ativos(book_data, period, dropdown, historical_data):
     df_book = pd.DataFrame(book_data)
     df_hist = pd.DataFrame(historical_data)
 
@@ -344,8 +354,10 @@ def atualizar_cards_ativos(book_data, period, historical_data):
     seta_caindo = ['fa fa-angle-down', 'textoTerciarioVermelho']
 
     lista_valores_ativos = []
+    lista_tags = []
     for ativo in range(len(dfativos)):
         tag_ativo = dfativos.iloc[ativo][0]
+        lista_tags.append(tag_ativo)
         valor_ativo = dfativos.iloc[ativo][1]
         variacao_ativo = dfativos.iloc[ativo][2]
         if variacao_ativo < 0:
@@ -353,19 +365,66 @@ def atualizar_cards_ativos(book_data, period, historical_data):
         else: 
             lista_valores_ativos.append([tag_ativo, valor_ativo, variacao_ativo, seta_crescendo[0], seta_crescendo[1]])
 
+
+    #Graficos
+    
     # print('CHEGOU AQUI')
-    # print(lista_valores_ativos)
+    # print(dropdown)
     # import pdb
     # pdb.set_trace()
+
+    df_hist = pd.DataFrame(historical_data)
+    df_hist['datetime'] = pd.to_datetime(df_hist['datetime'], format='%Y-%m-%d %H:%M:%S')
+    df_hist = slice_df_timedeltas(df_hist, period)
+
+    
+    
+
+    df_hist = df_hist[df_hist['symbol'].str.contains('|'.join(lista_tags))]
+
+    lista_graficos = []
+    for n, ticker in enumerate(lista_tags):
+        fig = go.Figure()
+        df_aux = df_hist[df_hist.symbol.str.contains(ticker)]
+        df_aux.dropna(inplace=True)
+        df_aux.close = df_aux.close / df_aux.close.iloc[0] - 1
+
+        fig.add_trace(go.Scatter(x=df_aux.datetime, y=df_aux.close*100, mode='lines', name=ticker,line=dict(color=CARD_GRAPHS_LINE_COLOR), hoverinfo = "skip"))
+
+        fig.update_layout(MAIN_CONFIG_3, showlegend=False, paper_bgcolor='rgba(0,0,0,0)', plot_bgcolor='rgba(0,0,0,0)')
+        fig.update_xaxes(visible=False)
+        fig.update_yaxes(visible=False)
+        
+        lista_graficos.append(fig)
+
+    # import pdb
+    # pdb.set_trace()
+
+
+
+
+  
     lista_colunas = []
     if len(lista_valores_ativos) <= 4:
         for i in range(len(lista_valores_ativos)):
             col = dbc.Col([
                         dbc.Card([
                             dbc.CardBody([
-                                html.Legend(lista_valores_ativos[i][0], className='textoQuartenario'),
-                                html.H5(["R$",lista_valores_ativos[i][1], " "], className='textoTerciario'),
-                                html.H5([html.I(className=lista_valores_ativos[i][3]), " ", lista_valores_ativos[i][2].round(2), "%"], className=lista_valores_ativos[i][4])
+                                dbc.Row([
+                                    dbc.Col([
+                                        html.Legend(lista_valores_ativos[i][0], className='textoQuartenario'),
+                                        
+                                    ], md=4),
+                                    dbc.Col([
+                                        dcc.Graph(figure=lista_graficos[i], config={"displayModeBar": False, "showTips": False}, className='graph_cards'),
+                                    ], md=8)
+                                ]),
+                                dbc.Row([
+                                    dbc.Col([
+                                        html.H5(["R$",lista_valores_ativos[i][1], " "], className='textoTerciario'),
+                                        html.H5([html.I(className=lista_valores_ativos[i][3]), " ", lista_valores_ativos[i][2].round(2), "%"], className=lista_valores_ativos[i][4])
+                                    ])
+                                ])
                             ])
                         ],className='cards_linha2'), 
                     ], md=3)
@@ -376,9 +435,21 @@ def atualizar_cards_ativos(book_data, period, historical_data):
             col = dbc.Col([
                         dbc.Card([
                             dbc.CardBody([
-                                html.Legend(lista_valores_ativos[i][0], className='textoQuartenario'),
-                                html.H5(["R$",lista_valores_ativos[i][1], " "], className='textoTerciario'),
-                                html.H5([html.I(className=lista_valores_ativos[i][3]), " ", lista_valores_ativos[i][2].round(2), "%"], className=lista_valores_ativos[i][4])
+                                dbc.Row([
+                                    dbc.Col([
+                                        html.Legend(lista_valores_ativos[i][0], className='textoQuartenario'),
+                                        
+                                    ], md=4),
+                                    dbc.Col([
+                                        dcc.Graph(figure=lista_graficos[i], config={"displayModeBar": False, "showTips": False}, className='graph_cards'),
+                                    ], md=8)
+                                ]),
+                                dbc.Row([
+                                    dbc.Col([
+                                        html.H5(["R$",lista_valores_ativos[i][1], " "], className='textoTerciario'),
+                                        html.H5([html.I(className=lista_valores_ativos[i][3]), " ", lista_valores_ativos[i][2].round(2), "%"], className=lista_valores_ativos[i][4])
+                                    ])
+                                ])
                             ])
                         ],className='cards_linha2'), 
                     ], md=3)
--- menu_styles.py
+++ menu_styles.py
@@ -2,13 +2,13 @@ COR_LEGENDA = 'rgba(255,255,255,0.4)'
 
 #menu radar graph
 INDICADORES = 'rgba(255,255,255,0.4)'
-BACKGROUND_RADAR = 'rgba(93, 59, 151, 1)'  
+BACKGROUND_RADAR = 'rgba(178, 45, 178, 1)'  
 LINHA_X = 'white'
 LINHA_Y = 'white'
-LINHAS_CIRCULARES = 'black'
-LINHA_CIRCULAR_EXTERNA = 'black'
+LINHAS_CIRCULARES = '#0d0834'
+LINHA_CIRCULAR_EXTERNA = '#0d0834'
 LINHAS_PREENCHIMENTO_1 = 'white'
-LINHAS_PREENCHIMENTO_2 = 'black'
+LINHAS_PREENCHIMENTO_2 = '#0d0834'
 CAIXA_LEGENDA = 'rgba(255,255,255,0.1)'
 AXIS_X_VALUES_COLOR = 'rgba(0,0,0,0)'
 TAMANHO_INDICADORES = 15
@@ -16,16 +16,16 @@ TAMANHO_RADAR = 190
 
 #menu line graph
 AXIS_FONT_SIZE = 20
-AXIS_VALUES_COLOR = 'rgba(255,255,255,0.4)'
+AXIS_VALUES_COLOR = 'white'
 LINHAS_DE_GRADE = 'rgba(255,255,255,0.1)'
 LINHA_ZERO_X = 'rgba(255,255,255,0.2)'
-LINHA_EVOLUCAO_PATRIMONIAL = 'rgba(93, 59, 151, 1)' 
-LISTA_DE_CORES_LINHAS = ['#4c00b0', 'rgba(93, 59, 151, 1)', '#670067', '#8a00c2', '#57356a', '#b100cd', '#be2ed6', 'white']
+LINHA_EVOLUCAO_PATRIMONIAL = '#b22db2' 
+LISTA_DE_CORES_LINHAS = ['#b22db2', 'rgba(76,0,176,0.4)' , 'rgba(103,0,103,0.4)', 'rgba(138,0,176,0.4)', 'rgba(87,53,106,0.4)', 'rgba(177,0,205,0.4)', 'rgba(190,46,214,0.4)', 'white']
 HOVER_LINE_GRAPH = {
-        "bgcolor":"#2a2a2a",
+        "bgcolor":"rgba(32, 36, 73, 0.7)",
         "font" : {'color':COR_LEGENDA}
 }
-PREENCHIMENTO_LINE_GRAPH = 'rgba(93, 59, 151, 0.5)'
+PREENCHIMENTO_LINE_GRAPH = 'rgba(178, 45, 178, 0.1)'
 
 
 
@@ -47,7 +47,21 @@ MAIN_CONFIG = {
 }
 MAIN_CONFIG_2 = {
     "hovermode": "x unified",
-    "legend": {"bgcolor": CAIXA_LEGENDA},
+    "legend": {"yanchor":"top", 
+                "y":0.25, 
+                "xanchor":"left",
+                "x":0.05,
+                "bgcolor": CAIXA_LEGENDA},
     "font" : {'color': COR_LEGENDA},
     "margin": {"l":0, "r":0, "t":10, "b":0},
 }
+
+
+MAIN_CONFIG_3 = {
+    # "hovermode": "x unified",
+    "legend": {"bgcolor": 'rgba(0,0,0,0)'},
+    "font" : None,
+    "margin": {"l":0, "r":0, "t":0, "b":0},
+}
+
+CARD_GRAPHS_LINE_COLOR = 'rgba(110, 83, 255, 0.3)'
